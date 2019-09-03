ecc6c63d4994cabd0b5cc366e5c8cabd84fe49df
suryansh <bhadouri@adobe.com>
2019-09-03 23:53:17 +0530

    adding return this in Helpers.groovy

--- src/main/java/com/campaign/App.java
+++ src/main/java/com/campaign/App.java
@@ -3,35 +3,35 @@ package com.campaign;
 public class App {
     public boolean m0() throws InterruptedException {
         long l = System.currentTimeMillis();
-        Thread.sleep(300000);
+        Thread.sleep(3000);
         System.out.println("Time taken M0:" + (System.currentTimeMillis() - l));
         return true;
     }
 
     public boolean m1() throws InterruptedException {
         long l = System.currentTimeMillis();
-        Thread.sleep(300000);
+        Thread.sleep(3000);
         System.out.println("Time taken M1:" + (System.currentTimeMillis() - l));
         return true;
     }
 
     public boolean m2() throws InterruptedException {
         long l = System.currentTimeMillis();
-        Thread.sleep(300000);
+        Thread.sleep(3000);
         System.out.println("Time taken M2:" + (System.currentTimeMillis() - l));
         return true;
     }
 
     public boolean m3() throws InterruptedException {
         long l = System.currentTimeMillis();
-        Thread.sleep(300000);
+        Thread.sleep(3000);
         System.out.println("Time taken M3:" + (System.currentTimeMillis() - l));
         return true;
     }
 
     public boolean m4() throws InterruptedException {
         long l = System.currentTimeMillis();
-        Thread.sleep(300000);
+        Thread.sleep(3000);
         System.out.println("Time taken M4:" + (System.currentTimeMillis() - l));
         return true;
     }
